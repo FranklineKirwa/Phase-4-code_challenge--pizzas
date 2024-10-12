32ec5f699c5654e09920551dc4ebbb4f31536484
franklynekirwa2 <sangkirwa2@gmail.com>
2024-10-12 15:51:25 +0300

    add

--- server/app.py
+++ server/app.py
@@ -2,8 +2,8 @@
 
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response, jsonify
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+#from flask_restful import Api, Resource
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
